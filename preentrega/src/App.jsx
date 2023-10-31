4ed1f7ac07f40d34f9cd88934ba2d26b33425549
RamaAcosta <rama.acosta.12@gmail.com>
2023-10-30 22:45:16 -0300

    PreEntrega1Acosta

---
+++ preentrega/src/App.jsx
@@ -0,0 +1,16 @@
+
+import { ItemListCointainer } from './components/ItemListContainer'
+import { NavBar } from './components/NavBar'
+
+function App() {
+
+
+  return (
+    <>
+     <NavBar  />
+     <ItemListCointainer  />
+    </>
+  )
+}
+
+export default App
---
+++ preentrega/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+export const NavBar = () => {
+    return (
+      <div className="navbar">
+        <ul>
+          <li><a href="">Inicio</a></li>
+          <li><a href="">Acerca de</a></li>
+          <li><a href="">Servicios</a></li>
+          <li><a href="">Contacto</a></li>
+        </ul>
+      </div>
+    );
+  };
+  
+  export default app;
